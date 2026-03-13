1e0c9694cf2264940796f548d822dedb46170aa0
Mehedi Hassan <idehemnassah@gmail.com>
2026-03-13 11:32:09 +0600

    update email link in Footer component to open Gmail directly

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -39,7 +39,7 @@ export default function Footer() {
               },
               {
                 label: "Email",
-                href: "mailto:idehemnassah@gmail.com",
+                href: "https://mail.google.com/mail/?view=cm&to=idehemnassah@gmail.com",
                 icon: (
                   <svg
                     className="w-5 h-5"
